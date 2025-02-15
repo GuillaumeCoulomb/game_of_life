95b5ba788e0eedbb69e175a76034ab086fa019e5
Guillaume Coulomb <guillaume.coulomb@etu.minesparis.psl.eu>
2025-02-15 19:57:57 +0100

    séparation du code en plusieurs fichiers

---
+++ game_of_life/game_of_life/cells.py
@@ -0,0 +1,86 @@
+ALIVE=True
+DEAD=False
+
+import pygame
+
+class Board:
+    def __init__(self,initial_pattern_path,output_path):
+
+        self._initial_pattern_path=initial_pattern_path
+        self._output_path=output_path
+
+        self._cells={} #the cells will be instantied in this dict
+        with open(self._initial_pattern_path, 'r') as fichier: # opening file in reading mode
+            lignes = fichier.readlines()
+            for i in range(len(lignes)):
+                for j in range(len(lignes[i].strip())):
+                    if lignes[i].strip()[j]=='0':
+                        self._cells[(i,j)]=Cell(i,j,DEAD) #the cells are instantied in self._cells, their keys are their positions
+                    if lignes[i].strip()[j]=='1':
+                        self._cells[(i,j)]=Cell(i,j,ALIVE)
+        
+
+    def step_forward(self): #moves the board to the next step
+
+        for elt in self._cells.values(): #implement the game of life principle
+            n_b=elt.number_of_neighboors(self)
+            if elt._current_state==ALIVE:
+                if n_b==0 or n_b==1 :
+                    elt._next_state=DEAD
+                if n_b==2 or n_b==3 :
+                    elt._next_state=ALIVE
+                if n_b>3:
+                    elt._next_state=DEAD
+            
+            if elt._current_state==DEAD:
+                if n_b==3:
+                    elt._next_state=ALIVE
+            
+        for elt in self._cells.values():
+            elt._current_state=elt._next_state
+
+
+    def output_file(self):
+
+        #calulate the size of the output file
+        n_raws=max([c._x_pos for c in self._cells.values()])+1
+        n_columns=max([c._y_pos for c in self._cells.values()])+1
+
+        # opening file in writing mode
+        with open(self._output_path, 'w') as fichier:
+            for i in range(n_raws):
+                for j in range(n_columns):
+                    if self._cells[(i,j)]._current_state==ALIVE: 
+                        fichier.write("1")
+                    if self._cells[(i,j)]._current_state==DEAD:     
+                        fichier.write("0")
+
+                fichier.write("\n") #line break at the end of each line
+
+
+
+class Cell(Board):
+    def __init__(self,x_pos,y_pos,state):
+        self._x_pos=x_pos
+        self._y_pos=y_pos
+        self._current_state=state #ALIVE or DEAD
+        self._next_state=state #used to calculate a new step
+
+    def number_of_neighboors(self,board):
+        n=0
+        circle=[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+        for elt in circle: #travels the 8 positions surrounding a cell
+            tested_position=self._x_pos + elt[0],self._y_pos + elt[1]
+            if (tested_position) in board._cells.keys() and board._cells[tested_position]._current_state==ALIVE :
+                n=n+1 
+        return n
+
+    def draw(self, screen):
+        if self._current_state==ALIVE:
+            color=(0,0,0)
+        if self._current_state==DEAD:
+            color=(255,255,255)
+        rect = pygame.Rect(self._y_pos * 10, self._x_pos * 10, 10, 10)
+        pygame.draw.rect(screen, color, rect)
+
+
--- game_of_life/game_of_life/main.py
+++ game_of_life/game_of_life/main.py
@@ -2,6 +2,10 @@ import argparse
 import pygame
 import sys
 
+from .display import Display
+from .cells import Board
+from .cells import Cell
+
 #python -m pip install pygame
 #poetry add pygame
 #poetry run game_of_life -i="my_input_file.txt" -o="my_output_file.text" -m=5 -f=0 --width=800 --height=600
@@ -25,112 +29,15 @@ args = parser.parse_args() # to call an arg : args.i or args.o ...
 
 ALIVE=True
 DEAD=False
-
-class Board:
-    def __init__(self,initial_pattern):
-        self._cells={} #the cells will be instantied in this dict
-        with open(initial_pattern, 'r') as fichier: # opening file in reading mode
-            lignes = fichier.readlines()
-            for i in range(len(lignes)):
-                for j in range(len(lignes[i].strip())):
-                    if lignes[i].strip()[j]=='0':
-                        self._cells[(i,j)]=Cell(i,j,DEAD) #the cells are instantied in self._cells, their keys are their positions
-                    if lignes[i].strip()[j]=='1':
-                        self._cells[(i,j)]=Cell(i,j,ALIVE)
-        
-
-    def step_forward(self): #moves the board to the next step
-
-        for elt in self._cells.values(): #implement the game of life principle
-            n_b=elt.number_of_neighboors(self)
-            if elt._current_state==ALIVE:
-                if n_b==0 or n_b==1 :
-                    elt._next_state=DEAD
-                if n_b==2 or n_b==3 :
-                    elt._next_state=ALIVE
-                if n_b>3:
-                    elt._next_state=DEAD
-            
-            if elt._current_state==DEAD:
-                if n_b==3:
-                    elt._next_state=ALIVE
-            
-        for elt in self._cells.values():
-            elt._current_state=elt._next_state
-
-
-    def output_file(self):
-
-        #calulate the size of the output file
-        n_raws=max([c._x_pos for c in self._cells.values()])+1
-        n_columns=max([c._y_pos for c in self._cells.values()])+1
-
-        # opening file in writing mode
-        with open(args.o, 'w') as fichier:
-            for i in range(n_raws):
-                for j in range(n_columns):
-                    if self._cells[(i,j)]._current_state==ALIVE: 
-                        fichier.write("1")
-                    if self._cells[(i,j)]._current_state==DEAD:     
-                        fichier.write("0")
-
-                fichier.write("\n") #line break at the end of each line
-
-class Cell(Board):
-    def __init__(self,x_pos,y_pos,state):
-        self._x_pos=x_pos
-        self._y_pos=y_pos
-        self._current_state=state #ALIVE or DEAD
-        self._next_state=state #used to calculate a new step
-
-    def number_of_neighboors(self,board):
-        n=0
-        circle=[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
-        for elt in circle: #travels the 8 positions surrounding a cell
-            tested_position=self._x_pos + elt[0],self._y_pos + elt[1]
-            if (tested_position) in board._cells.keys() and board._cells[tested_position]._current_state==ALIVE :
-                n=n+1 
-        return n
-
-    def draw(self, screen):
-        if self._current_state==ALIVE:
-            color=(0,0,0)
-        if self._current_state==DEAD:
-            color=(255,255,255)
-        rect = pygame.Rect(self._y_pos * 10, self._x_pos * 10, 10, 10)
-        pygame.draw.rect(screen, color, rect)
-
-
-class Display:
-
-    def __init__(self,width,height):
-        pygame.init()
-
-        self._width=width
-        self._height=height
-        self._cell_size=10
-        self._clock= pygame.time.Clock()
-        screen_size = (int(self._width),int(self._height))
-
-        self._screen = pygame.display.set_mode(screen_size)
-        self._screen.fill((128,128,128))
-
-    def draw_update(self,board):
-        
-        for cell in board._cells.values():
-            cell.draw(self._screen) 
-
-        pygame.display.update()
-        self._clock.tick(int(args.f))
         
  
 def game_of_life():
     
-    board=Board(args.i)
+    board=Board(args.i,args.o)
     
     if int(args.d)==1: #if display on, initiates pygame and instantiate Display
         pygame.init()
-        display=Display(int(args.width),int(args.height))
+        display=Display(int(args.width),int(args.height),int(args.f))
     
     for k in range(int(args.m)):
         board.step_forward() #moves the cells according to the game of life principles
@@ -143,6 +50,7 @@ def game_of_life():
     if int(args.d)==1: #if display on, stops pygame
         pygame.quit()
         quit()
+        sys.exit()
         
     board.output_file() #writes the output file representing the final board a txt file
     
